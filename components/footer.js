36e5f62cf6a38cd97d539d8900b2a2e9428046e9
Ericka Diaz <erickaldiaz@gmail.com>
2021-09-02 20:12:17 -0300

    delete panchito

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-3xl lg:text-2xl font-bold tracking-tighter leading-tight text-left lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             {/* Statically Generated with Next.js. */}
-          @2021 Ericka Diaz 🌭 
+          @2021 Ericka Diaz 
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
